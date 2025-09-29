00c9e1dcc60621cdd9cb230eab00df4bf8c29050
kubar123 <kubar123@users.noreply.github.com>
2025-09-29 12:51:37 +1000

    Formatting: Removed empty whitespace

--- src/main/java/com/lansoftprogramming/runeSequence/sequence/SequenceManager.java
+++ src/main/java/com/lansoftprogramming/runeSequence/sequence/SequenceManager.java
@@ -61,69 +61,56 @@ public class SequenceManager {
 
 			activeSequence.processDetections(results);
 		} else {
-
 			System.out.println("SequenceManager: No active sequence to process detections");
 		}
 	}
 
 	public synchronized List<DetectionResult> getCurrentAbilities() {
 		if (activeSequence == null) {
-
 			System.out.println("SequenceManager.getCurrentAbilities: No active sequence");
 			return List.of();
 		}
 		List<DetectionResult> current = activeSequence.getCurrentAbilities();
-
 		System.out.println("SequenceManager.getCurrentAbilities: " + current.size() + " abilities");
 		return current;
 	}
 
 	public synchronized List<DetectionResult> getNextAbilities() {
 		if (activeSequence == null) {
-
 			System.out.println("SequenceManager.getNextAbilities: No active sequence");
 			return List.of();
 		}
 		List<DetectionResult> next = activeSequence.getNextAbilities();
-
 		System.out.println("SequenceManager.getNextAbilities: " + next.size() + " abilities");
 		return next;
 	}
 
 	public synchronized void addNamedSequence(String name, SequenceDefinition def) {
-
 		System.out.println("SequenceManager: Adding named sequence: " + name);
 		namedSequences.put(name, def);
 	}
 
 	public synchronized boolean activateSequence(String name) {
-
 		System.out.println("SequenceManager: Activating sequence: " + name);
-
 		SequenceDefinition def = namedSequences.get(name);
 		if (def == null) {
-
 			System.out.println("SequenceManager: Sequence not found: " + name);
-
 			System.out.println("Available sequences: " + namedSequences.keySet());
 			return false;
 		}
 
 		this.activeSequence = new ActiveSequence(def, abilityConfig);
-
 		System.out.println("SequenceManager: Sequence activated successfully");
 		return true;
 	}
 
 	public synchronized void resetActiveSequence() {
-
 		System.out.println("SequenceManager: Resetting active sequence");
 		if (activeSequence != null) activeSequence.reset();
 	}
 
 	public synchronized boolean hasActiveSequence() {
 		boolean hasActive = activeSequence != null;
-
 		System.out.println("SequenceManager.hasActiveSequence: " + hasActive);
 		return hasActive;
 	}
